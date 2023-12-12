3fd57038ced6eb914e52b21af3793406e83aa68f
Brianca Knight <briancaknight@gmail.com>
2023-12-12 11:32:04 -0800

    pass test two creating an ADD_NOTE reducer
    Co-Authored-by:Zuri Gallegos <Zurisadailopezmartinez@gmail.com>

--- src/__tests__/reducers/note-list-reducer.test.js
+++ src/__tests__/reducers/note-list-reducer.test.js
@@ -1,7 +1,40 @@
 import noteListReducer from '../../reducers/note-list-reducer';
 
 describe('noteListReducer', () => {
+
+  let action;
+  const noteData = {
+    text: 'Hi there!',
+    author: 'Zuri G',
+    upvotes: '1',
+    downvotes: '1',
+    id: 1
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(noteListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should successfully add new note data to mainNoteList', () => {
+    const { text, author, upvotes, downvotes, id } = noteData;
+    action = {
+      type: 'ADD_NOTE',
+      text: text,
+      author: author,
+      upvotes: upvotes,
+      downvotes: downvotes,
+      id: id
+    };
+
+    expect(noteListReducer({}, action)).toEqual({
+      [id] : {
+        text: text,
+        author: author,
+        upvotes: upvotes,
+        downvotes: downvotes,
+        id: id
+      }
+    });
+  });
+
 });
--- src/reducers/note-list-reducer.js
+++ src/reducers/note-list-reducer.js
@@ -1,5 +1,20 @@
 const reducer = (state = {}, action) => {
-return state;
+  const { text, author, upvotes, downvotes, id } = action;
+  switch (action.type) {
+    case 'ADD_NOTE':
+      return Object.assign({}, state, {
+        [id]: {
+          text: text, 
+          author: author, 
+          upvotes: upvotes, 
+          downvotes: downvotes,
+          id: id
+        }
+      });
+      default: 
+      return state;
+  }
 };
+
 
 export default reducer;
