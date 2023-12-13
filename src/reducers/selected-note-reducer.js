9878d4730e6cf9259473066842d6bee477e17a9f
Brianca Knight <briancaknight@gmail.com>
2023-12-12 16:28:35 -0800

    WIP add selected-note reducer to app
    Co-Authored-by:Zuri Gallegos <Zurisadailopezmartinez@gmail.com>

--- src/reducers/selected-note-reducer.js
+++ src/reducers/selected-note-reducer.js
@@ -1,5 +1,18 @@
 const reducer = (state = {}, action) => {
-return state;
+  const { text, author, upvotes, downvotes, id } = action;
+  switch (action.type) {
+    case 'SELECT_NOTE':
+      return ({}, state, {
+        [id]: {
+          text: text,
+          author: author,
+          upvotes: upvotes,
+          downvote: downvotes,
+          id: id
+        }
+      });
+  }
+  return state;
 };
 
 export default reducer;
